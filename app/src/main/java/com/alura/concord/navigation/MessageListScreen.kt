086fb1d01f8752f657ca3ba5a6db30ce955a645b
Junior <35709152+git-jr@users.noreply.github.com>
2023-04-16 11:00:20 -0300

    Clean version

--- app/src/main/java/com/alura/concord/navigation/MessageListScreen.kt
+++ app/src/main/java/com/alura/concord/navigation/MessageListScreen.kt
@@ -1,22 +1,12 @@
 package com.alura.concord.navigation
 
-import androidx.activity.compose.ManagedActivityResultLauncher
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.alura.concord.extensions.checkImagePermission
-import com.alura.concord.extensions.getPathAndName
-import com.alura.concord.extensions.getPath
-import com.alura.concord.extensions.showMessage
-import com.alura.concord.medias.loadExternalImages
 import com.alura.concord.ui.chat.MessageListViewModel
 import com.alura.concord.ui.chat.MessageScreen
 import com.alura.concord.ui.components.ModalBottomSheetFile
@@ -34,19 +24,6 @@ fun NavGraphBuilder.messageListScreen(
         backStackEntry.arguments?.getString(messageChatIdArgument)?.let { chatId ->
             val viewModelMessage = hiltViewModel<MessageListViewModel>()
             val uiState by viewModelMessage.uiState.collectAsState()
-            val context = LocalContext.current
-
-
-            val requestPermissionLauncher: ManagedActivityResultLauncher<String, Boolean> =
-                rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-                    if (isGranted) {
-                        viewModelMessage.setImagePermission(true)
-                        viewModelMessage.setShowBottomSheetSticker(true)
-                        context.showMessage("Permissões concedidas")
-                    } else {
-                        context.showMessage("Permissões ainda não concedidas")
-                    }
-                }
 
             MessageScreen(
                 state = uiState,
@@ -59,12 +36,7 @@ fun NavGraphBuilder.messageListScreen(
 
                 },
                 onShowSelectorStickers = {
-                    context.checkImagePermission(
-                        requestPermissionLauncher = requestPermissionLauncher,
-                        onPermissionHasObtained = {
-                            viewModelMessage.setImagePermission(true)
-                            viewModelMessage.setShowBottomSheetSticker(true)
-                        })
+                    viewModelMessage.setShowBottomSheetSticker(true)
                 },
                 onDeselectMedia = {
                     viewModelMessage.deselectMedia()
@@ -75,9 +47,7 @@ fun NavGraphBuilder.messageListScreen(
             )
 
             if (uiState.showBottomSheetSticker) {
-                val stickerList = loadExternalImages(LocalContext.current)
                 ModalBottomSheetSticker(
-                    stickerList,
                     onSelectedSticker = {
                         viewModelMessage.setShowBottomSheetSticker(false)
                         viewModelMessage.loadMediaInScreen(path = it.toString())
@@ -87,45 +57,13 @@ fun NavGraphBuilder.messageListScreen(
                     })
             }
 
-            val selectorMediaFiles =
-                rememberLauncherForActivityResult(
-                    ActivityResultContracts.OpenDocument(),
-                    onResult = {
-                        it?.let { uri ->
-                            context.getPathAndName(uri, onFound = { path, name ->
-                                if (uiState.messageValue.isEmpty()) {
-                                    uiState.onMessageValueChange(name)
-                                }
-                                viewModelMessage.loadMediaInScreen(path = path)
-                                viewModelMessage.sendMessage()
-                            })
-                        }
-                    })
-
-            val selectorMediaImages =
-                rememberLauncherForActivityResult(
-                    ActivityResultContracts.PickVisualMedia(),
-                    onResult = {
-                        it?.let { uri ->
-                            context.getPath(
-                                uri = uri,
-                                onFoundPath = { path ->
-                                    viewModelMessage.loadMediaInScreen(path)
-                                }
-                            )
-                        }
-                    }
-                )
-
             if (uiState.showBottomSheetFile) {
                 ModalBottomSheetFile(
                     onSelectPhoto = {
                         viewModelMessage.setShowBottomSheetFile(false)
-                        selectorMediaImages.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
                     },
                     onSelectFile = {
                         viewModelMessage.setShowBottomSheetFile(false)
-                        selectorMediaFiles.launch(arrayOf("*/*")) // Others: arrayOf("application/pdf", "image/png")
                     }, onBack = {
                         viewModelMessage.setShowBottomSheetFile(false)
                     })
--- app/src/main/java/com/alura/concord/ui/components/BottomSheetStickers.kt
+++ app/src/main/java/com/alura/concord/ui/components/BottomSheetStickers.kt
@@ -1,7 +1,6 @@
 package com.alura.concord.ui.components
 
 import android.net.Uri
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -19,13 +18,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.alura.concord.R
-import com.alura.concord.extensions.toThumbnail
-import com.alura.concord.extensions.toUri
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ModalBottomSheetSticker(
-    stickerList: MutableList<Long>,
+    stickerList: MutableList<Long> = mutableListOf<Long>(),
     onSelectedSticker: (Uri) -> Unit = {},
     onBack: () -> Unit = {}
 ) {
@@ -83,7 +80,7 @@ private fun BottomSheetStickers(
                     Modifier
                         .fillMaxSize()
                         .padding(8.dp)
-                        .clickable { onSelectedSticker(item.toUri()) }
+//                        .clickable { onSelectedSticker() }
                 ) {
 
                     coil.compose.AsyncImage(
@@ -91,7 +88,7 @@ private fun BottomSheetStickers(
                             .size(50.dp)
                             .align(Alignment.Center),
                         contentScale = ContentScale.Inside,
-                        model = context.toThumbnail(item),
+                        model = "",
                         placeholder = painterResource(R.drawable.image_place_holder),
                         error = painterResource(R.drawable.image_place_holder),
                         contentDescription = null,
